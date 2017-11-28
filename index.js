42383c34d21534fdd493035e4422631caff8b477
Stephen Thurman <thurman024@gmail.com>
2017-11-28 02:58:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = "I don't do much."
 
 function destructivelyAppendKitten(kitten) {
+  kittens.shift(kitten)
+  return kittens
+}
+
+function destructivelyPrependKitten(kitten) {
   kittens.unshift(kitten)
   return kittens
 }
